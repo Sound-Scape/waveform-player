c4bd09ffdf048d4c5be1a1d554bd91bea8eea800
Trey Pernell <trey@piazza.com>
2018-09-11 11:32:21 -0700

    Recieve/format data in client using axios

--- client/components/WaveformPlayer.jsx
+++ client/components/WaveformPlayer.jsx
@@ -1,4 +1,5 @@
 const React = require('react');
+const axios = require('axios');
 const InfoContainer = require('./InfoContainer.jsx');
 const TitleContainer = require('./TitleContainer.jsx');
 const PlayerContainer = require('./PlayerContainer.jsx');
@@ -8,17 +9,32 @@ class WaveformPlayer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      song: [],
+      comments: [],
       isPlaying: false,
     };
   }
 
+  componentDidMount() {
+    const songId = Math.floor(Math.random() * 100) + 1;
+    axios.get(`http://localhost:3003/song/${songId}`)
+      .then(({ data }) => {
+        this.setState({
+          song: data.allData.songData[0],
+          comments: data.allData.commentData,
+        });
+      });
+  }
+
   render() {
+    const { song, comments, isPlaying } = this.state;
+
     return (
       <div className="waveform-player-wrapper">
-        <InfoContainer />
-        <TitleContainer />
-        <PlayerContainer />
-        <ArtContainer />
+        <InfoContainer song={song} />
+        <TitleContainer song={song} isPlaying={isPlaying} />
+        <PlayerContainer comments={comments} isPlaying={isPlaying} />
+        <ArtContainer song={song} />
       </div>
     );
   }
